0cc3a5e230171e03a7cd2926b203ee5da623d688
Ye Huang <sysuhuangye@gmail.com>
2016-04-04 03:07:19 +0300

    Install dev-dependencies

--- system.config.js
+++ system.config.js
@@ -14,9 +14,11 @@ System.config({
   },
 
   map: {
+    "angular": "github:angular/bower-angular@1.3.14",
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
+    "jquery": "npm:jquery@2.2.2",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
