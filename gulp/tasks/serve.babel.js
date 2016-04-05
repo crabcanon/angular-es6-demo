be1da9934223c0f8b651492fe22c710a0559249b
Ye Huang <sysuhuangye@gmail.com>
2016-04-05 23:29:04 +0300

    Add comments for gulp tasks

--- gulp/tasks/serve.babel.js
+++ gulp/tasks/serve.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import historyApiFallback from 'connect-history-api-fallback';
 
+// Serve the webapp in port http://localhost:9000
 gulp.task('serve', ['watch'], done => {
    browserSync({
      open: true,
--- gulp/tasks/watch.babel.js
+++ gulp/tasks/watch.babel.js
@@ -4,10 +4,12 @@ import browserSync from 'browser-sync';
 
 const reload = browserSync.reload;
 
+// Report function
 function message(event) {
   console.log('File ' + event.path + ' was ' + event.type);
 }
 
+// Watch changes of scripts, html templates and styles
 gulp.task('watch', ['build'], () => {
   gulp.watch([paths.sources], ['scripts', reload]).on('change', message);
   gulp.watch([paths.html], ['html', reload]).on('change', message);
