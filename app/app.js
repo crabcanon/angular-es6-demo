df4f77e1cee6122634e8bf80a05a93aa683206b9
Ye Huang <sysuhuangye@gmail.com>
2016-04-06 04:23:26 +0300

    Add comments to app.js

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
+// Import dependencies
 import $ from 'jquery';
 import angular from 'angular';
 
+// Import directive modules
 import timestampModule from './timestamp/timestamp.module';
 import popupModule from './popup/popup.module';
 import timerModule from './timer/timer.module';
 
+// Construct angular app module via dependency injection
 let appModule = angular.module('kiosked', [timestampModule.name, popupModule.name, timerModule.name]);
 
+// Bootstrap webapp
 angular.element(document).ready(function() {
   angular.bootstrap(document, [appModule.name], {
     strictDi: true
