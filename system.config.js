23224b86d5ad96e0b1a2503d6be5c82f6755f735
Ye Huang <sysuhuangye@gmail.com>
2016-04-08 15:35:42 +0300

    Remove jquery

--- system.config.js
+++ system.config.js
@@ -18,7 +18,6 @@ System.config({
     "babel": "npm:babel-core@5.8.38",
     "babel-runtime": "npm:babel-runtime@5.8.38",
     "core-js": "npm:core-js@1.2.6",
-    "jquery": "npm:jquery@2.2.2",
     "text": "github:systemjs/plugin-text@0.0.7",
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
